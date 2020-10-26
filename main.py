5480468b2b8cfb79fa7bf3b55bb03f78549eefc8
Bopemt <Bopemt@users.noreply.github.com>
2020-10-26 22:19:08 +0900

    Delete all code from main.py

--- main.py
+++ main.py
@@ -6,12 +6,6 @@ from Subject import Subject
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print(CrossSection.FirstSection.value)
-    education_year = EducationYear(begin_year=2020, end_year=2022)
-    print(education_year.end_year, education_year.begin_year)
-    group = Group(name='FIIT-20', year=2020)
-    print(group.name, group.year)
-    subject = Subject(code='al', name='algebra')
-    print(subject.code, subject.name)
+    pass
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
