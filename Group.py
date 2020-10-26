f6ca7ec3c38e2467a0e2ddea4ed0c4e82550f17a
Bopemt <Bopemt@users.noreply.github.com>
2020-10-26 20:31:23 +0900

    Add group class

---
+++ Group.py
@@ -0,0 +1,4 @@
+class Group:
+    def __init__(self, name: str, year: int):
+        self.name = name
+        self.year = year
--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import sqlalchemy
 from CrossSection import CrossSection
 from EducationYear import EducationYear
+from Group import Group
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print(CrossSection.FirstSection.value)
     education_year = EducationYear(begin_year=2020, end_year=2022)
     print(education_year.end_year, education_year.begin_year)
+    group = Group(name='FIIT-20', year=2020)
+    print(group.name, group.year)
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
