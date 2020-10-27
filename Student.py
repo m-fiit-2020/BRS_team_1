551a17016e5a2f261a8dbd33d776b8d9d7b4b775
Dayana <dayankoo201297@gmail.com>
2020-10-27 21:14:47 +0300

    new block

---
+++ Student.py
@@ -0,0 +1,7 @@
+class Student:
+    def __init__(self, code: str, fio: int, birthdate: str, email: str, phone: str):
+        self.code = code
+        self.fio = fio
+        self.birthdate = birthdate
+        self.email = email
+        self.phone = phone
