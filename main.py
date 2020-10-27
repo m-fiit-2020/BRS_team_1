0f40731a0a5946615ba8fa0be114abbf66e62b2e
VasilevSaryal <43399842+VasilevSaryal@users.noreply.github.com>
2020-10-27 19:28:56 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ from Subject import Subject
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    pass
+    print('Hello M-FIIT-20')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
