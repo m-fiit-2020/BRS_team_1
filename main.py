c2b6defe4ea90d9bf24cdd1e5fc288362a284c8c
Bopemt <Bopemt@users.noreply.github.com>
2020-10-29 20:13:27 +0900

    Add class Main and empty containers for functions in main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import sqlalchemy
 from CrossSection import CrossSection
 from EducationYear import EducationYear
 from Group import Group
@@ -8,4 +7,50 @@ from Subject import Subject
 if __name__ == '__main__':
     print('Hell M-FIIT-20')
 
+
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+class Main:
+    def print_menu(self):
+        pass
+
+    def print_group_menu(self):
+        pass
+
+    def print_subject_menu(self):
+        pass
+
+    def add_group(self):
+        pass
+
+    def edit_group(self):
+        pass
+
+    def delete_group(self):
+        pass
+
+    def add_student(self):
+        pass
+
+    def edit_student(self):
+        pass
+
+    def remove_student(self):
+        pass
+
+    def add_subject(self):
+        pass
+
+    def edit_subject(self):
+        pass
+
+    def delete_subject(self):
+        pass
+
+    def add_brs_point(self):
+        pass
+
+    def edit_brs_point(self):
+        pass
+
+    def delete_brs_point(self):
+        pass
