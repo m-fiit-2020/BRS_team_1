3fc7eed42b1a6403bf78847cf05e3462b97669ea
Dayana <dayankoo201297@gmail.com>
2020-12-12 18:35:35 +0900

    fix

---
+++ Tests/Factory/StudentTest.py
@@ -0,0 +1,16 @@
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+    def test_create_student_case_1(self):
+        # given
+        code = "160978"
+        fio = "Васильев Сарыал Иннокентьевич"
+        birthdate = "12.07.1998"
+        email = "example@mail.ru"
+        phone = "79969141275"
+
+
+
+if __name__ == '__main__':
+    unittest.main()
