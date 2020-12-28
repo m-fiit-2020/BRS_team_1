80e81ae7cd94fcd6486a41792460765e93d5fa62
Alekey_Egorov <eavamga@gmail.com>
2020-12-28 19:05:17 +0900

    test Checker update

--- Tests/Controller/Checker/Old/test_checker.py
+++ Tests/Controller/Checker/Old/test_checker.py
@@ -7,7 +7,6 @@ from Models.CrossSection import CrossSection
 from Models.Group import Group
 from Models.Subject import Subject
 
-
 class TestChecker(unittest.TestCase):
     def test_check_email(self):
         list_emails = ("vladimirp1998@gmail.com", "eremenkoira19981998@gmail.com", "fhljkjhuh@gmail.com",
@@ -40,53 +39,73 @@ class TestChecker(unittest.TestCase):
         for j in list_wrong_phones:
             self.assertEqual(Checker.check_phone(j), False)
 
-    @unittest.skip("Work in progress..")
     def test_check_points(self):
         group1 = Group("М-ФИИТ-20", 2020)
         group2 = Group("М-ФИИТ-19", 2019)
 
         year1 = EducationYear(2020, 2021)
-        year2 = EducationYear(2021, 2022)
 
         subject1 = Subject("321", "Психология")
-        subject2 = Subject("322", "Методология")
         subject3 = Subject("323", "Английский")
 
-        cross_section1 = CrossSection("Первый контрольный срез")
-        cross_section2 = CrossSection("Второй контрольный срез")
-        cross_section3 = CrossSection("Заключительный контрольный срез")
+        cross_section1 = CrossSection.FirstSection
+        cross_section2 = CrossSection.SecondSection
+        cross_section3 = CrossSection.FinalSection
+
+
 
         student1 = Student(code=123, fio="Егоров Алексей Васильевич", birthdate="01. 04. 1996",
                            email="eavamga@gmail.com", phone="+79244669579", group=group1)
         student2 = Student(code=124, fio="Иванов Иван Иванович", birthdate="30. 12. 2000", email="example@gmail.com",
                            phone="+79991112233", group=group2)
 
-        EgorovAV_s1_y1_cs1 = BRSPoints(subject=subject1, year=year1, cross_section=cross_section1, points=99,
+
+        EgorovAV_cs1 = BRSPoints(subject=subject1, year=year1, cross_section=cross_section1, points=33,
                                        student=student1)
-        EgorovAV_s2_y2_cs1 = BRSPoints(subject=subject2, year=year2, cross_section=cross_section1, points=17,
+        EgorovAV_cs2 = BRSPoints(subject=subject1, year=year1, cross_section=cross_section2, points=66,
                                        student=student1)
+        EgorovAV_cs3 = BRSPoints(subject=subject1, year=year1, cross_section=cross_section3, points=99,
+                                 student=student1)
 
-        IvamovII_s1_y1_cs1 = BRSPoints(subject=subject1, year=year1, cross_section=cross_section1, points=20,
-                                       student=student2)
-        IvamovII_s2_y2_cs1 = BRSPoints(subject=subject2, year=year2, cross_section=cross_section1, points=77,
-                                       student=student2)
 
-        EgorovAV_s3_y1_cs1 = BRSPoints(subject=subject3, year=year1, cross_section=cross_section1, points=102,
+
+        IvamovII_cs1 = BRSPoints(subject=subject1, year=year1, cross_section=cross_section1, points=20,
+                                       student=student2)
+        IvamovII_cs2 = BRSPoints(subject=subject1, year=year1, cross_section=cross_section2, points=20,
+                                       student=student2)
+        IvamovII_cs3 = BRSPoints(subject=subject1, year=year1, cross_section=cross_section3, points=20,
+                                 student=student2)
+
+
+
+        EgorovAV_cs1_wrong = BRSPoints(subject=subject3, year=year1, cross_section=cross_section1, points=101,
                                        student=student1)
-        EgorovAV_s3_y1_cs2 = BRSPoints(subject=subject3, year=year1, cross_section=cross_section2, points=-12,
+        EgorovAV_cs2_wrong = BRSPoints(subject=subject3, year=year1, cross_section=cross_section2, points=0,
                                        student=student1)
+        EgorovAV_cs3_wrong = BRSPoints(subject=subject3, year=year1, cross_section=cross_section3, points=0,
+                                 student=student1)
 
-        IvamovII_s3_y1_cs1 = BRSPoints(subject=subject3, year=year1, cross_section=cross_section1, points=0.22,
+
+
+        IvamovII_cs1_wrong = BRSPoints(subject=subject3, year=year1, cross_section=cross_section1, points=65,
                                        student=student2)
-        IvamovII_s3_y1_cs2 = BRSPoints(subject=subject3, year=year1, cross_section=cross_section2, points=True,
+        IvamovII_cs2_wrong = BRSPoints(subject=subject3, year=year1, cross_section=cross_section2, points=60,
                                        student=student2)
+        IvamovII_cs3_wrong = BRSPoints(subject=subject3, year=year1, cross_section=cross_section3, points=30,
+                                 student=student2)
 
-        self.assertEqual(Checker.check_points(EgorovAV_s1_y1_cs1, year1, cross_section1), True)
-        self.assertEqual(Checker.check_points(EgorovAV_s2_y2_cs1, year2, cross_section1), True)
-        self.assertEqual(Checker.check_points(IvamovII_s1_y1_cs1, year1, cross_section1), True)
-        self.assertEqual(Checker.check_points(IvamovII_s2_y2_cs1, year2, cross_section1), True)
 
-        self.assertEqual(Checker.check_points(EgorovAV_s3_y1_cs1, year1, cross_section1), False)
-        self.assertEqual(Checker.check_points(EgorovAV_s3_y1_cs2, year1, cross_section2), False)
-        self.assertEqual(Checker.check_points(IvamovII_s3_y1_cs1, year1, cross_section1), False)
-        self.assertEqual(Checker.check_points(IvamovII_s3_y1_cs2, year1, cross_section2), False)
+        Egorov_subject1_2020_2021 = [EgorovAV_cs1, EgorovAV_cs2, EgorovAV_cs3]
+        Egorov_subject3_2020_2021_wrong = [EgorovAV_cs1_wrong, EgorovAV_cs2_wrong, EgorovAV_cs3_wrong]
+
+        Ivanov_subject1_2020_2021 = [IvamovII_cs1, IvamovII_cs2, IvamovII_cs3]
+        Ivanov_subject1_2020_2021_wrong = [IvamovII_cs1_wrong, IvamovII_cs2_wrong, IvamovII_cs3_wrong]
+
+        self.assertEqual(Checker.check_points(Egorov_subject1_2020_2021, cross_section3, EgorovAV_cs3.points), True)
+        self.assertEqual(Checker.check_points(Ivanov_subject1_2020_2021, cross_section3, IvamovII_cs3.points), True)
+
+        self.assertEqual(Checker.check_points(Egorov_subject3_2020_2021_wrong, cross_section1, EgorovAV_cs1_wrong.points), False)
+        self.assertEqual(Checker.check_points(Ivanov_subject1_2020_2021_wrong, cross_section3, IvamovII_cs3_wrong.points), False)
+
+
+     
